50a44394ab2a58e19f2bfe80a6f16bda192d0b4a
RicardoTeofilo <ricardopachec38@gmail.com>
2022-12-23 16:42:46 -0300

    Adding the integration tests to the price WebClient

--- P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package com.udacity.pricing;
 
+import com.udacity.pricing.domain.price.Price;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +9,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import static org.junit.Assert.assertTrue;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureJsonTesters
 public class PricingServiceApplicationTests {
 
@@ -20,9 +23,55 @@ public class PricingServiceApplicationTests {
 	public void contextLoads() {
 	}
 
+	@Test
 	public void getPriceSuccess(){
 
+		webTestClient.get()
+				.uri(uriBuilder ->
+						uriBuilder
+						.path("services/price/")
+						.queryParam("vehicleIdentificationNumber", "123456ABC")
+						.build())
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody(Price.class);
+	}
 
+
+	/**
+	 * Test that two calls to the get price service with the same vehicle
+	 * identification number will return the same price value
+	 */
+	@Test
+	public void getSamePriceForSameVehicleNumber(){
+
+		String vehicleIdentificationNumber = "123456ABC";
+
+		Price firstPrice = webTestClient.get()
+				.uri(uriBuilder ->
+						uriBuilder
+								.path("services/price/")
+								.queryParam("vehicleIdentificationNumber", vehicleIdentificationNumber)
+								.build())
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody(Price.class)
+				.returnResult()
+				.getResponseBody();
+
+		Price secondPrice = webTestClient.get()
+				.uri(uriBuilder ->
+						uriBuilder
+								.path("services/price/")
+								.queryParam("vehicleIdentificationNumber", vehicleIdentificationNumber)
+								.build())
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody(Price.class)
+				.returnResult()
+				.getResponseBody();
+
+		assertTrue(firstPrice.getPrice().compareTo(secondPrice.getPrice()) == 0);
 	}
 
 }
